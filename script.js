ee841d1d8c9e328c6b9b50ab2b93451961ecdfcd
SHUBARUUU <joshuaemmanuelbalois@gmail.com>
2025-03-24 09:21:28 +0800

    Updated the Navbar and ADDED a PNC Favicon 3-24-25

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
+// Hero Slider
+const slides = document.querySelectorAll(".hero-slide");
+const dots = document.querySelectorAll(".hero-slider .btn");
+let currentSlide = 0;
 
-    // Hero Slider
-    const slides = document.querySelectorAll('.hero-slide');
-    const dots = document.querySelectorAll('.hero-slider .btn');
-    let currentSlide = 0;
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+  });
+  dots.forEach((dot, i) => {
+    dot.classList.toggle("btn-light", i !== index);
+    dot.classList.toggle("btn-warning", i === index);
+  });
+}
 
-    function showSlide(index) {
-      slides.forEach((slide, i) => {
-        slide.classList.toggle('active', i === index);
-      });
-      dots.forEach((dot, i) => {
-        dot.classList.toggle('btn-light', i !== index);
-        dot.classList.toggle('btn-warning', i === index);
-      });
-    }
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
 
-    function nextSlide() {
-      currentSlide = (currentSlide + 1) % slides.length;
-      showSlide(currentSlide);
-    }
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    currentSlide = i;
+    showSlide(currentSlide);
+  });
+});
 
-    dots.forEach((dot, i) => {
-      dot.addEventListener('click', () => {
-        currentSlide = i;
-        showSlide(currentSlide);
-      });
-    });
+setInterval(nextSlide, 5000);
 
-    setInterval(nextSlide, 5000);
+// Campus Tour
+const locations = document.querySelectorAll(".campus-location");
+const locationDetails = document.querySelector(".campus-tour-details");
 
-    // Campus Tour
-    const locations = document.querySelectorAll('.campus-location');
-    const locationDetails = document.querySelector('.campus-tour-details');
-
-    locations.forEach(location => {
-      location.addEventListener('click', () => {
-        locations.forEach(l => l.classList.remove('active'));
-        location.classList.add('active');
-        // Update location details here
-      });
-    });
- 
+locations.forEach((location) => {
+  location.addEventListener("click", () => {
+    locations.forEach((l) => l.classList.remove("active"));
+    location.classList.add("active");
+    // Update location details here
+  });
+});
